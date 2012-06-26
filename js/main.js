1baca675cf66d4701467d525cefc4b92ff4baff8
abiyasa <ilham.suhardi@gmail.com>
2012-06-26 18:14:09 +0200

    update with bigger icons

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
-// Author: Thomas Davis <thomasalwyndavis@gmail.com>
-// Filename: main.js
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, forin: true, maxerr: 50, regexp: true */
+/*global define, $, FileError, brackets, window, require */
 
 // Require.js allows us to configure shortcut alias
 require.config({
-  paths: {
-    jquery: 'libs/jquery/jquery-min',
-    underscore: 'libs/underscore/underscore',
-    backbone: 'libs/backbone/backbone',
-    text: 'libs/require/text'
-  }
-
+    paths: {
+        jquery: 'libs/jquery/jquery-min',
+        underscore: 'libs/underscore/underscore',
+        backbone: 'libs/backbone/backbone',
+        text: 'libs/require/text'
+    }
 });
 
-require(['views/app'], function(AppView){
-  var app_view = new AppView;
+require(['views/app'], function (AppView) {
+    'use strict';
+    var app_view = new AppView();
 });
--- js/models/todo.js
+++ js/models/todo.js
@@ -1,29 +1,36 @@
-define(['underscore', 'backbone'], function(_, Backbone) {
-  var TodoModel = Backbone.Model.extend({
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, forin: true, maxerr: 50, regexp: true */
+/*global define, $, FileError, brackets, window */
+define(['underscore', 'backbone'], function (_, Backbone) {
+    'use strict';
+    var TodoModel = Backbone.Model.extend({
 
-    // Default attributes for the todo.
-    defaults: {
-      content: "empty todo...",
-      done: false
-    },
+        // Default attributes for the todo.
+        defaults: {
+            content: "empty todo...",
+            done: false
+        },
 
-    // Ensure that each todo created has `content`.
-    initialize: function() {
-      if (!this.get("content")) {
-        this.set({"content": this.defaults.content});
-      }
-    },
+        // Ensure that each todo created has `content`.
+        initialize: function () {
+            if (!this.get("content")) {
+                this.set({
+                    "content": this.defaults.content
+                });
+            }
+        },
 
-    // Toggle the `done` state of this todo item.
-    toggle: function() {
-      this.save({done: !this.get("done")});
-    },
+        // Toggle the `done` state of this todo item.
+        toggle: function () {
+            this.save({
+                done: !this.get("done")
+            });
+        },
 
-    // Remove this Todo from *localStorage*.
-    clear: function() {
-      this.destroy();
-    }
+        // Remove this Todo from *localStorage*.
+        clear: function () {
+            this.destroy();
+        }
 
-  });
-  return TodoModel;
+    });
+    return TodoModel;
 });
